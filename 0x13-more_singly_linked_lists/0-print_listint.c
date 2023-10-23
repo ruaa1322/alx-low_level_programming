227d4c730b9c785f82f7d1bb6c4d9b78f4526df4
ruaa1322 <ruaa000@hotmail.com>
2023-10-23 13:32:44 -0700

    ff

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print listint - prints a linked lists
+ * @h: pointer to first node
+ * *
+ * Return: sizeof list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h->next;
+		i++;
+	}
+	return (i);
+}
